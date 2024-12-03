ad762d911017a6b9eaca922f19d337bee4c904e1
Laura Dean <43610011+lldean18@users.noreply.github.com>
2024-12-03 11:12:50 +0000

    Create job_submission_script.bash

---
+++ job_submission_script.bash
@@ -0,0 +1,33 @@
+#!/bin/bash
+# file intended for running on the UoN HPC Ada
+
+#SBATCH --partition=defq
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=10g
+#SBATCH --time=01:00:00
+#SBATCH --job-name=bam2fqzip
+#SBATCH --output=/gpfs01/home/USERNAME/slurm-%x-%j.out
+
+# load software modules
+module load samtools-uoneasy/1.18-GCC-12.3.0
+
+# set variables
+bam=~/LIFE4137-HPC_practice/stickleback.bam
+
+# index the bam file
+samtools index $bam
+
+# convert the bam file to fastq format
+# -O output qulaity tags if they exist
+# -t output RG, BC and QT tags to the FASTQ header line
+# then pipe to gzip to compress the fastq file
+samtools bam2fq \
+-O \
+-t \
+--threads 4 \
+$bam | gzip > ${bam%.*}.fastq.gz
+
+# unload modules
+module unload samtools-uoneasy/1.18-GCC-12.3.0
